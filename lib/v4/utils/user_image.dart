5c56b8cada17ec69e61eb6b3dad685a97e28f4eb
Mat Grzegorzek <mateusz.grzegorzek@rivia.net>
2026-04-27 11:57:23 +0200

    Fix userImage display

--- lib/v4/utils/user_image.dart
+++ lib/v4/utils/user_image.dart
@@ -1,4 +1,6 @@
 import 'package:amity_sdk/amity_sdk.dart';
+import 'package:amity_uikit_beta_service/v4/core/styles.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import '../core/theme.dart';
@@ -21,47 +23,60 @@ class AmityUserImage extends StatelessWidget {
   String get displayName => user?.displayName ?? "";
 
   double _mapSizeToFontSize(double size) {
-    if (size == 64) return 32;
-    if (size == 56) return 32;
-    if (size == 40) return 20;
-    if (size == 32) return 17;
-    if (size == 28) return 15;
-    if (size == 16) return 10;
+    final maxFontSize = size * 0.3;
+
+    if (size == 64) return maxFontSize > 32 ? 32 : maxFontSize;
+    if (size == 56) return maxFontSize > 32 ? 32 : maxFontSize;
+    if (size == 40) return maxFontSize > 20 ? 20 : maxFontSize;
+    if (size == 32) return maxFontSize > 17 ? 17 : maxFontSize;
+    if (size == 28) return maxFontSize > 13 ? 13 : maxFontSize;
+    if (size == 16) return maxFontSize > 10 ? 10 : maxFontSize;
     return 10; // default case
   }
 
   Widget _buildPlaceholder() {
     return Container(
-      color: theme.primaryColor.blend(ColorBlendingOption.shade2),
+      decoration: BoxDecoration(
+        color: theme.avatarBackgroundColor,
+        border: Border.all(color: theme.avatarBorderColor, width: 1.0),
+        shape: BoxShape.circle,
+      ),
       child: Center(
         child: Text(
-          displayName.trim().isNotEmpty ? displayName.trim()[0].toUpperCase() : '?',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: _mapSizeToFontSize(size),
-            fontWeight: FontWeight.normal,
-          ),
+          _getInitials(),
+          style: AmityTextStyle.custom(_mapSizeToFontSize(size), FontWeight.w400, theme.avatarTextColor)
         ),
       ),
     );
   }
 
+  String _getInitials() {
+    if (displayName.trim().isEmpty) {
+      return 'A';
+    }
+
+    final nameParts = displayName.trim().split(' ');
+    if (nameParts.length == 1) {
+      return nameParts[0][0].toUpperCase();
+    } else {
+      return (nameParts[0][0] + nameParts[1][0]).toUpperCase();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    if (imageUrl != null && imageUrl!.isNotEmpty) {
+    if (imageUrl != null && imageUrl!.isNotEmpty && !kDebugMode) {
       return Image.network(
         imageUrl!,
         fit: BoxFit.cover,
-        loadingBuilder: (BuildContext context, Widget child,
-            ImageChunkEvent? loadingProgress) {
+        loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
           if (loadingProgress == null) {
             return child;
           } else {
             return _buildPlaceholder();
           }
         },
-        errorBuilder:
-            (BuildContext context, Object error, StackTrace? stackTrace) {
+        errorBuilder: (BuildContext context, Object error, StackTrace? stackTrace) {
           return _buildPlaceholder();
         },
       );
